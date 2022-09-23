c4f3a24214e7cc3f03f8caeeb4dd05c7f6953f99
theteus29 <104210687+theteus29@users.noreply.github.com>
2022-09-23 09:08:07 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //receber 4 numeros e fazer a som//
+    Scanner ler = new Scanner (System.in);
+    int num, som=0, i=1;
+    System.out.print("Esse é um sistama que soma 4 numeros inteiros.\n\n");
+      do{
+        System.out.print("Informe o "+i+"º numero inteiro: ");
+        num=ler.nextInt();
+        som+=num;
+        i++ ;    
+      }
+      while(i!=5);
+    System.out.print("\n\nA soma dos numeros digitados é: "+som);    
   }
 }
